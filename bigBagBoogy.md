e43bc5b444b1a94c538d0c5baa72a9aea7c0de6c
maarten <van_den_bergmaarten@hotmail.com>
2023-07-19 19:19:55 +0300

    test added

--- bigBagBoogy.md
+++ bigBagBoogy.md
@@ -5,3 +5,5 @@ git branch -M main
 git add .
 git commit -m "test added"
 git push -u origin main
+
+The keyboard shortcut to toggle word wrap is Alt+Z
--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -13,5 +13,55 @@ pragma solidity ^0.8.18;
  */
 
 contract DSCEngine {
+    ///////////////////
+    // Errors
+    ///////////////////
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+    error DSCEngine__needsMoreThanZero();
+    error DSCEngine__onlyWETHorWBTCAllowed();
+     ///////////////////
+    // State Variables
+    ///////////////////
 
+mapping(address token => address priceFeed) private s_priceFeeds;
+
+
+    ///////////////////
+    // Modifiers
+    ///////////////////
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__needsMoreThanZero();
+        }
+        _;
+    }
+    modifier isAllowedToken(address token) {
+        address weth = 00xbb9bc244d798123fde783fcc1c72d3bb8c189413;
+        address wbtc = 0x0164431354;
+        if (tokenSent != weth || wbtc) {
+            revert DSCEngine__onlyWETHorWBTCAllowed();
+        }
+        _;
+    }
+    ///////////////////
+    // Functions
+    ///////////////////
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+        }
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+    }
+
+    ///////////////////
+    // External Functions
+    ///////////////////
+
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 _amountCollateral
+    ) external moreThanZero(_amountCollateral) {}
 }
