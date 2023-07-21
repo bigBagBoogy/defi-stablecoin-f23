70d1d379b5924bdcda2a52b3432af7e9b16c2e30
maarten <van_den_bergmaarten@hotmail.com>
2023-07-21 10:07:03 +0300

    test added

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -31,7 +31,7 @@ contract DSCEngine is ReentrancyGuard {
 
     mapping(address token => address priceFeed) private s_priceFeeds;
     //mapping (who(what => how many))collateralDeposited
-    mapping(address user => mapping(address collateraltoken => uint256 amount)) private s_collateralDeposited;
+    mapping(address user => mapping(address collateralToken => uint256 amount)) private s_collateralDeposited;
     /// @dev Amount of DSC minted by user
     mapping(address user => uint256 amount) private s_DSCMinted;
     DecentralizedStableCoin private immutable i_dsc;
@@ -84,11 +84,11 @@ contract DSCEngine is ReentrancyGuard {
      */
     function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
 s_DSCMinted[msg.sender] += amountDscToMint
-
-
-require they have sufficient collateral
-appoint the coins to the user
-update by emitting "minted(amount, coin, to whom)"
+//If they minted too much (DSC $150, ETH $100)
+_revertIfHealthFactorIsBroken(msg.sender);
+//require they have sufficient collateral
+//appoint the coins to the user.
+//update by emitting "minted(amount, coin, to whom)"
     }
      /*
      * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
@@ -114,20 +114,64 @@ update by emitting "minted(amount, coin, to whom)"
             revert DSCEngine__TransferFailed();
         }
     }
+///////////////////
+    // Public Functions
+    ///////////////////
+
+ ///////////////////
+    // Private Functions
+    ///////////////////
+
+
      //////////////////////////////
     // Private & Internal View & Pure Functions
+    // Internal _functions _get a _leading _underscore!!!
     //////////////////////////////
+function _getAccountInformation(address user) private view returns (uint256 totalDscMinted, uint256 collateralValueInUsd) {
+totalDscMinted = s_DSCMinted[user];
+collateralValueInUsd = getAccountCollateralValue(user);
+}
+
     /*
     * Returns how close to liquidation a user is
     * If a user goes below 1, then they can get liqidated
     */
 function _healthFactor(address user) private view {
-// total DSC minted
-// total collateral VALUE
+// need get total DSC minted
+// need get total collateral VALUE
+(uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+}
+
+
+function _getUsdValue(address token, uint amount) private view returns (uint256) {
+
 }
 
     function _revertIfHealthFactorIsBroken(address user) internal view {
 // 1. Check health factor (do they have enough collateral?)
 // 2. revert if they don't
+(uint256 totalDscMinted,/*debt*/ uint256 collateralValueInUsd) = getAccountInformation(user);
+    }
+        ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    // External & Public View & Pure Functions
+    ////////////////////////////////////////////////////////////////////////////
+
+//there's a _getUsdValue AND a getUsdValue function (without underscore).
+// a helper function used internally within the contract to calculate a value, while getUsdValue might be a function designed to provide the calculated USD value to external callers (a public getter function).
+    function getUsdValue(address token, uint256 amount /* in WEI*/) external view returns (uint256) {
+return _getUsdValue(token, amount);
+    }
+
+    function getAccountCollateralValue(address user) public view returns(uint256 totalCollateralValueInUsd) {
+        // loop through each collateral token in the s_collateralDeposited mapping,  
+        // get the amount they have deposited, "deconstruct the elements..."
+        // and map it to the price, to get the USD value. I would say 
+        for(uint256 i = 0; i < s_collateralDeposited.length; i++) {
+            address token = s_collateralDeposited[i]; //get token
+            uint256 amount = s_collateralDeposited[amount][token]; //of the token get the amt.
+            totalCollateralValueInUsd += _getUsdValue(token, amount);
+        }
+        return totalCollateralValueInUsd;
     }
 }
