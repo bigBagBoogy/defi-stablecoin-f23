36d9b6dd18ccaa8e1521528f568074d8ced71db7
maarten <van_den_bergmaarten@hotmail.com>
2023-07-22 11:30:01 +0300

    test added

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,12 +4,25 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeployDSC is Script {
-    function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {}
-vm.startBroadcast();
-DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-DSCEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
-vm.stopBroadcast();
-    constructor() {}
+    address[] tokenAddresses;
+    address[] priceFeedAddresses;
+
+    function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
+        HelperConfig config = new HelperConfig(); // here we initialize a new custom variable "HelperConfig" named "config" and set it equal to the output of the HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(dscEngine));
+        vm.stopBroadcast();
+        return (dsc, dscEngine, config);
+    }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -6,35 +6,41 @@ import {MockV3Aggregator} from "../test/Mocks/MockV3Aggregator.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     struct NetworkConfig {
         address wethUsdPriceFeed;
         address wbtcUsdPriceFeed;
         address weth;
         address wbtc;
-        address deployerKey;
-
+        uint256 deployerKey;
     }
-    uint8 public constant DECIMALS = 8;
-    int256 public constant ETH_USD_PRICE = 2000e8;
-    int256 public constant BTC_USD_PRICE = 1000e8;
 
-    NetworkConfig public activeNetworkConfig;
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaNetworkConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilNetworkConfig();
+        }
+    }
 
-    constructor() {}
-        function getSepoliaNetworkConfig() public view returns (NetworkConfig memory) {
-            return NetworkConfig({
+    function getSepoliaNetworkConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
             wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
             weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
             wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
             deployerKey: vm.envUint("PRIVATE_KEY")
-            })
-        }
-
+        });
+    }
 
     function getOrCreateAnvilNetworkConfig() public returns (NetworkConfig memory) {
         // it seems weird to me this says "wethUsdPriceFeed" and not "ethUsdPriceFeed"
-        if (activeNetworkConfig.wethUsdPriceFeed != address(0)){ 
+        if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
             return activeNetworkConfig;
         }
         vm.startBroadcast();
@@ -44,14 +50,12 @@ contract HelperConfig is Script {
         MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_PRICE);
         ERC20Mock wbtcMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
         vm.stopBroadcast();
-           return NetworkConfig({
-            wethUsdPriceFeed: address(ethUsdPriceFeed), 
+        return NetworkConfig({
+            wethUsdPriceFeed: address(ethUsdPriceFeed),
             wbtcUsdPriceFeed: address(btcUsdPriceFeed),
             weth: address(wethMock),
             wbtc: address(wbtcMock),
             deployerKey: vm.envUint("PRIVATE_KEY")
-            })
-
-
+        });
     }
-
+}
